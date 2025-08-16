460a1f76f66157f25057c9930902a219b1c87056
unknown <luanacardosos@outlook.com>
2025-08-16 16:47:54 -0300

    feat: adiciona função adicionarAmigos

--- app.js
+++ app.js
@@ -1,1 +1,9 @@
-//O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
+const inputAmigo = document.getElementById ("amigo");
+const listaAmigos = []
+const ulListaAmigos = document.getElementById ("listaAmigos");
+
+function adicionarAmigo() {
+    listaAmigos.push(inputAmigo.value);
+    ulListaAmigos.innerHTML += `<li>${inputAmigo.value}</li>`;
+
+};
